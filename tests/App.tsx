33e9824d8fbd08043e058dd8e939362a6115eac2
LouisKamp <louiskampeskildsen99@gmail.com>
2021-12-19 21:18:07 +0100

    init

---
+++ tests/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+    const kage = `hej + ${5}`
+    return (
+        <h1>
+            <h2>
+                {kage}
+            </h2>
+        </h1>
+    )
+}
+
+export default App
